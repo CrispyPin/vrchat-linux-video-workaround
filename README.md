f7924b574d0a503dea45b5f2e00c8e6e0ce4aed9
CrispyPin <crispin@tasa.se>
2022-03-11 18:40:34 +0100

    add missing dependency to readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Dumb workaround that allows you to play video player content from a vrchat world in mpv instead, since they don't work in proton yet.
 
 ## setup
-requires `mpv python3 mono`
+requires `mpv yt-dlp python3 mono`
 
 build+install with `make install`  
 run `vrc-mpv.py` to play videos with mpv when they are requested by vrchat
--- src/vrc-video-url-handler.sh
+++ src/vrc-video-url-handler.sh
@@ -5,7 +5,7 @@ true >> vrc-video-url-handler.log
 echo $1 > /tmp/VRC_VIDEO_URL
 
 # if you need the raw url back in vrc (also compile with RETURN_URL)
-# you also need to place yt-dlp in the vrchat install folder since the path is not accessible.
+# you also need to place yt-dlp in the vrchat install folder since the system path is not accessible.
 
 #./yt-dlp --no-cache-dir --rm-cache-dir -f 'mp4[height<=?1080]/best[height<=?1080]' --get-url $1 > /tmp/VRC_VIDEO_RAW_URL
 
